fb7d28568f17d4e828c6a25d2f38684fcc8ae4c6
Matt Salisbury <matty8salisbury@gmail.com>
2022-09-24 19:38:01 +0100

    continue to debug ssh command

--- GCP-shinymenu-startup.sh
+++ GCP-shinymenu-startup.sh
@@ -30,9 +30,9 @@ gcloud beta compute instances create venuename-shinymenu-machine \
 #COPY ACROSS THE venueinfo.R FILE
 
 gcloud compute scp /home/shiny/OrderApp/venueinfo-venuename.R serviceAccount@venuename-shinymenu-machine:~/venueinfo.R --zone=europe-west1-b --quiet
-gcloud compute ssh serviceAccount@venuename-shinymenu-machine --zone=europe-west1-b --quiet venuename-shinymenu-machine --command "sudo mv -f ~/venueinfo.R /home/shiny/OrderApp/"
+gcloud compute ssh serviceAccount@venuename-shinymenu-machine --zone=europe-west1-b --quiet --command "sudo mv -f ~/venueinfo.R /home/shiny/OrderApp/"
 
 #COPY ACROSS THE priceList FILE
 
 gcloud compute scp /home/shiny/OrderApp/price_list-venuename.csv serviceAccount@venuename-shinymenu-machine:~/price_list.csv --zone=europe-west1-b --quiet
-gcloud compute ssh serviceAccount@venuename-shinymenu-machine --zone=europe-west1-b --quiet venuename-shinymenu-machine --command "sudo mv -f ~price_list.csv /home/shiny/OrderApp/"
+gcloud compute ssh serviceAccount@venuename-shinymenu-machine --zone=europe-west1-b --quiet --command "sudo mv -f ~price_list.csv /home/shiny/OrderApp/"
